cfc264e2595d017458f9058cef0da7ef10e12581
Tobias Kappé <tobias.kappe@gmail.com>
2017-02-25 15:33:51 +0000

    Initial commit.

---
+++ kagi.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+prompt () {
+    zenity --title "$1 - Kagi" --text="$1" ${@:2}
+}
+
+prompt-text() {
+    prompt "$1" --entry
+}
+
+prompt-error() {
+    prompt "$1" --error
+}
+
+prompt-confirm() {
+    zenity "$1" --question
+}
+
+get-key-path() {
+    echo "$KAGI_BASEDIR/$1"
+}
+
+check-id() {
+    [ "$1" == "${1//[^a-zA-Z0-9\-]/}" ]
+}
+
+check-exists() {
+    [ -e "$(get-key-path "$1")" ]
+}
+
+check-overwrite() {
+    if check-exists "$1"; then
+        prompt-confirm "Key exists. Overwrite?"
+    else    
+        true
+    fi
+}
+
+get-key-id() {
+    local KEY_ID=$(prompt-text "Key ID")
+
+    if [ -z "$KEY_ID" ]; then
+        prompt-error "No ID given."
+        false
+    elif ! check-id "$KEY_ID"; then
+        prompt-error "Invalid ID."
+        false
+    else
+        echo "$KEY_ID"
+    fi
+}
+
+generate-key() {
+    pwgen $KAGI_PWGEN_FLAGS -N 1
+}
+
+gpg-encrypt() {
+    gpg -ea -r "$KAGI_GPG_KEY"
+}
+
+gpg-decrypt() {
+    gpg -q -d
+}
+
+to-clipboard() {
+    xclip $KAGI_XCLIP_FLAGS
+}
+
+if [ -z ${KAGI_GPG_KEY} ]; then
+    prompt-error "No GPG key configured."
+    exit 1
+fi
+
+if [ -z ${KAGI_PWGEN_FLAGS+x} ]; then
+    KAGI_PWGEN_FLAGS="-nys 32"
+fi
+
+if [ -z ${KAGI_XCLIP_FLAGS+x} ]; then
+    KAGI_XCLIP_FLAGS="-selection clipboard"
+fi
+
+if [ -z ${XDG_DATA_HOME+x} ]; then
+    XDG_DATA_HOME="${HOME}/.local/share"
+fi
+
+if [ -z ${KAGI_BASEDIR+x} ]; then
+    KAGI_BASEDIR="${XDG_DATA_HOME}/kagi"
+fi
+
+if [ ! -d "$KAGI_BASEDIR" ]; then
+    mkdir "$KAGI_BASEDIR"
+fi
+
+case "$1" in
+write)
+    KEY_ID=$(get-key-id) || exit 1
+
+    if check-overwrite "$KEY_ID"; then
+        KEY_PATH=$(get-key-path "$KEY_ID")
+        generate-key | gpg-encrypt > "$KEY_PATH"
+    fi
+    ;;
+read)
+    KEY_ID=$(get-key-id) || exit 1
+
+    if check-exists "$KEY_ID"; then
+        KEY_PATH=$(get-key-path "$KEY_ID")
+        gpg-decrypt < "$KEY_PATH" | to-clipboard
+    else
+        prompt-error "Unknown key"
+    fi
+    ;;
+esac
